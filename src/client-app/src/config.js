439d5428095d429dc67c4b878ca8f5fa1acb902a
Charris <c@harrischris.com>
2022-02-06 13:24:01 +0100

    Speak to spirits over the correct port

--- src/client-app/src/config.js
+++ src/client-app/src/config.js
@@ -1,8 +1,9 @@
 export const config = {
-    port: 8088,
-    transport: process.env.NODE_ENV === 'development' ? 'http' : 'https'
+    transport: process.env.NODE_ENV === 'development' ? 'http' : 'https',
+    port: process.env.NODE_ENV === 'development' ? ':8088' : ''
 }
 
 // switch the for prod to https
-export const getApiUrlString = (route) => `${config.transport}://${window.location.hostname}:${config.port}/api/${route}`
+export const getApiUrlString = (route) =>
+    `${config.transport}://${window.location.hostname}${config.port}/api/${route}`
 export const getApiUrl = (route) => new URL(getApiUrlString(route))
